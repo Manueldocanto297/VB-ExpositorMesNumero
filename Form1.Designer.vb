b77d1beacd9294ac4624fd4fa82a9c3c2f4ca913
Manuel <manueldocanto777@hotmail.com>
2020-05-05 18:57:46 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnAceptar = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnAceptar
+        '
+        Me.btnAceptar.Location = New System.Drawing.Point(128, 55)
+        Me.btnAceptar.Name = "btnAceptar"
+        Me.btnAceptar.Size = New System.Drawing.Size(75, 23)
+        Me.btnAceptar.TabIndex = 0
+        Me.btnAceptar.Text = "Aceptar"
+        Me.btnAceptar.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(53, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 13)
+        Me.Label1.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.ClientSize = New System.Drawing.Size(332, 101)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnAceptar)
+        Me.ForeColor = System.Drawing.Color.Black
+        Me.Name = "Form1"
+        Me.Text = "Expositor de Mes"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnAceptar As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+    Private Sub btnAceptar_Click(sender As Object, e As EventArgs) Handles btnAceptar.Click
+        Dim var As Integer
+        var = InputBox("Ingrese un numero del 1 al 12: ")
+        If (var >= 1 And var <= 12) Then
+            Select Case var
+                Case 1
+                    MsgBox("Enero")
+                Case 2
+                    MsgBox("Febrero")
+                Case 3
+                    MsgBox("Marzo")
+                Case 4
+                    MsgBox("Abril")
+                Case 5
+                    MsgBox("Mayo")
+                Case 6
+                    MsgBox("Junio")
+                Case 7
+                    MsgBox("Julio")
+                Case 8
+                    MsgBox("Agosto")
+                Case 9
+                    MsgBox("Septiembre")
+                Case 10
+                    MsgBox("Octubre")
+                Case 11
+                    MsgBox("Noviembre")
+                Case 12
+                    MsgBox("Diciembre")
+
+            End Select
+        Else
+            MsgBox("El numero ingresado no es correcto")
+
+        End If
+    End Sub
+End Class
